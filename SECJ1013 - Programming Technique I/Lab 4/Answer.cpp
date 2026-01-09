d0283da564ef70cdb95734304489ef40561474e0
Ng Xuan Yee <ngxuanyee@graduate.utm.my>
2026-01-09 17:59:49 +0800

    Update dynamic array handling in Answer.cpp
    Refactor code to use 'num' for dynamic array size and total calculation.

--- SECJ1013 - Programming Technique I/Lab 4/Answer.cpp
+++ SECJ1013 - Programming Technique I/Lab 4/Answer.cpp
@@ -1,31 +1,35 @@
+/*
+NAME: NG XUAN YEE 
+MATRIC NUMBER: A25CS0291
+DATE: 9 JANUARY 2026
+*/
+
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n;
-
-    cout << "Enter number of values:";
-    cin >> n;
+int main(){
+    int num;
+    cout<<"Enter number of values:";
+    cin>>num;
 
     // Dynamic memory allocation
-    int *arr = new int[n];
+    int *arr=new int[num];
 
-    cout << "Enter values in array:" << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> *(arr + i);   // access using pointer
+    cout<<"Enter values in array:" << endl;
+    for (int x=0;x<num;x++) {
+        cin>>*(arr+x);  
     }
 
     // Pointer for total
     int *total = new int;
     *total = 0;
 
-    for (int i = 0; i < n; i++) {
-        *total += *(arr + i);
+    for (int x=0;x<num;x++) {
+        *total+=*(arr+x);
     }
 
-    cout << "Total of the values in the array is " << *total << endl;
+    cout<<"Total of the values in the array is "<<*total<<endl;
 
-    // Free dynamic memory
+    // Delete all the dynamic memory allocation
     delete[] arr;
     delete total;
 
